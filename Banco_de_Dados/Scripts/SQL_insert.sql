897316db5abc28b44da56e5296598dd81f5f9805
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-31 11:49:49 -0300

    Add Script para Inserir Dados Multiplos no BD

--- Banco_de_Dados/Scripts/SQL_insert.sql
+++ Banco_de_Dados/Scripts/SQL_insert.sql
@@ -1,3 +1,5 @@
 SELECT * FROM materias
 
 INSERT INTO materias values(1, 'portugues', 'literatura e gramatica simples')
+
+INSERT INTO materias values(2, 'Matematica', 'geometria e algebra'),(3, 'Fisica', 'Volume e Sinestesia');
