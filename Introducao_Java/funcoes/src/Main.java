c109378dd2ae1eac20729697be7386e073e9fc2d
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-07 15:46:30 -0300

    Add Projeto de Funcoes de Introdução ao Java

---
+++ Introducao_Java/funcoes/src/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo 1
+        //   String nomeOriginal = "Gabriel Matheus - GIGANTE";
+        //   saudacao(nomeOriginal);
+        //}
+
+        // public static void saudacao(String nomeParametro) {
+        //   System.out.println("Ola " + nomeParametro);
+        // }
+
+        // Exemplo 2
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
